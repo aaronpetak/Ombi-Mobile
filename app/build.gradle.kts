b4fd192dc0bd7a1dd363eeede79feb21a0c20211
Aaron Petak <20822715+aaronpetak@users.noreply.github.com>
2026-02-17 22:00:55 -0800

    Android Studio theme upgrade and gradle updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.material)
 
     // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
